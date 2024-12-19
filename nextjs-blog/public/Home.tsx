9c328cd36f31135f3886e86d1eb811bfe6e67a5b
karlabelem <kbarrientosguevara@gmail.com>
2024-12-18 21:53:02 -0800

    Fixed formatting
    fixed formatting for clarity

--- nextjs-blog/public/Home.tsx
+++ nextjs-blog/public/Home.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import styles from '../styles/Home.module.css';
 
 type Props = {
-  openPage: (page: string) => void;
+    openPage: (page: string) => void;
 };
 
-export const Home: React.FC<Props> = ({ openPage }) => {
-  return (
-    <div>
-        <h2 className={styles.wordContent}>Welcome to the Event Planner!
-        </h2>
-        <p className={styles.wordContent}>
-            Organize, manage, and enjoy stress-free event planning with just a few clicks
-      </p>
-        <div className={styles.buttonContainer}>
-            <button className={styles.buttonClick} onClick={() => openPage("guestlist")}>Guest list</button>
-            <button className={styles.buttonClick} onClick={() => openPage("addguest")}>Add guest</button>
+export const Home: React.FC<Props> = ({openPage}) => {
+    return (
+        <div>
+            <h2 className={styles.wordContent}>Welcome to the Event Planner!
+            </h2>
+            <p className={styles.wordContent}>
+                Organize, manage, and enjoy stress-free event planning with just a few clicks
+            </p>
+            <div className={styles.buttonContainer}>
+                <button className={styles.buttonClick} onClick={() => openPage("guestlist")}>Guest list</button>
+                <button className={styles.buttonClick} onClick={() => openPage("addguest")}>Add guest</button>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
