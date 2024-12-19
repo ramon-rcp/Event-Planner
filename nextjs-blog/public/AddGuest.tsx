ca7f8c4e36b4e0bbaecf1db075809b898f34c1cd
karlabelem <kbarrientosguevara@gmail.com>
2024-12-18 21:52:02 -0800

    Updated styling
    Edited design of add guest page

--- nextjs-blog/public/AddGuest.tsx
+++ nextjs-blog/public/AddGuest.tsx
@@ -1,7 +1,7 @@
-import React, { ChangeEvent, Component, MouseEvent } from "react";
-import { Show } from "./App";
-import { save } from "./server";
-import styles from "../styles/Home.module.css"
+import React, {ChangeEvent, Component, MouseEvent} from "react";
+import {Show} from "./App";
+import {save} from "./server";
+import styles from "../styles/AddGuest.module.css"
 
 const KID_LIMIT: number = 20
 
@@ -31,7 +31,7 @@ type AgProps = {
 }
 
 export class AddGuest extends Component<AgProps, AgState> {
-    constructor(props: AgProps){
+    constructor(props: AgProps) {
         super(props)
 
         this.state = {
@@ -45,24 +45,24 @@ export class AddGuest extends Component<AgProps, AgState> {
 
     render = (): JSX.Element => {
         console.log(this.state.kidinfo?.names)
-        return <main>
-            <h2>
+        return <main className={styles.main}>
+            <h2 className={styles.titleStyle}>
                 Add Guest
             </h2>
             <p>
-                Name: 
+                Name:
                 <input type="text" value={this.state.name} onChange={this.updateName}></input>
             </p>
             <p>
                 Allergies:
-                <textarea rows={4} cols={60} value={this.state.allergies} onChange={this.updateAllergies}></textarea> 
+                <textarea rows={2} cols={60} value={this.state.allergies} onChange={this.updateAllergies}></textarea>
             </p>
             <p>
-                Plus One:
-                <input type="radio" name="plusone" value="1" 
-                    checked={this.state.plusone} onChange={this.updatePlusOne}></input> Yes 
-                <input type="radio" name="plusone" value="0" 
-                    checked={!this.state.plusone} onChange={this.updatePlusOne}></input> No             
+                Plus one:
+                <input type="radio" name="plusone" value="1"
+                       checked={this.state.plusone} onChange={this.updatePlusOne}></input> Yes
+                <input type="radio" name="plusone" value="0"
+                       checked={!this.state.plusone} onChange={this.updatePlusOne}></input> No
             </p>
             {this.renderPlusOne()}
             <p>
@@ -70,40 +70,45 @@ export class AddGuest extends Component<AgProps, AgState> {
                 <input type="number" value={this.state.kids} onChange={this.updateKids}></input>
             </p>
             {this.renderKids()}
-            <button onClick={this.addGuest}>Add</button><button onClick={this.goBack}>Back</button>
-            <p>{this.state.errormsg}</p>
+            <button className={styles.buttonContainer} onClick={this.addGuest}>Add</button>
+            <button className={styles.buttonContainer} onClick={this.goBack}>Back</button>
+            <p className={styles.errorMessage}> {this.state.errormsg}</p>
         </main>
     }
 
     renderKids = (): JSX.Element => {
         const html: JSX.Element[] = []
-        for(let i:number = 0; i<Number(this.state.kids); i++){
-            html.push(<div><p>
-                Kid {i+1} Name:
-                <input type="text" name={i.toString()} onChange={this.updateKidName} value={this.state.kidinfo?.names[i]}></input>
+        for (let i: number = 0; i < Number(this.state.kids); i++) {
+            html.push( <div className={styles.kidInfo}>
+                    <p>
+                Kid {i + 1} Name:
+                <input type="text" name={i.toString()} onChange={this.updateKidName}
+                       value={this.state.kidinfo?.names[i]}></input>
             </p>
-            <p>
-                Kid {i+1} Allergies:
-                <textarea rows={4} cols={60} name={i.toString()} value={this.state.kidinfo?.allergies[i]} onChange={this.updateKidAllergies}></textarea>
-            </p></div>)
+                <p>
+                    Kid {i + 1} Allergies:
+                    <textarea rows={4} cols={60} name={i.toString()} value={this.state.kidinfo?.allergies[i]}
+                              onChange={this.updateKidAllergies}></textarea>
+                </p></div>)
         }
         return <div>{html}</div>
     }
 
     renderPlusOne = (): JSX.Element => {
-        if(!this.state.plusone){
+        if (!this.state.plusone) {
             return <div></div>
         }
-        return <div>
+        return( <div>
             <p>
-                Plus One Name: 
+                Plus One Name:
                 <input type="text" value={this.state.plusoneinfo?.name} onChange={this.updatePoName}></input>
             </p>
             <p>
                 Plus One Allergies:
-                <textarea rows={4} cols={60} value={this.state.plusoneinfo?.allergies} onChange={this.updatePoAllergies}></textarea>
+                <textarea rows={4} cols={60} value={this.state.plusoneinfo?.allergies}
+                          onChange={this.updatePoAllergies}></textarea>
             </p>
-        </div>
+        </div> )
     }
 
     goBack = (evt: MouseEvent<HTMLButtonElement>): void => {
@@ -111,25 +116,25 @@ export class AddGuest extends Component<AgProps, AgState> {
     }
 
     addGuest = (evt: MouseEvent<HTMLButtonElement>): void => {
-        if(this.state.name === ""){
+        if (this.state.name === "") {
             this.setState({errormsg: "Name can't be blank"})
             return;
         }
-        if(this.state.plusoneinfo?.name === ""){
+        if (this.state.plusoneinfo?.name === "") {
             this.setState({errormsg: "Plus one's name can't be blank"})
             return;
         }
-        if(this.state.kidinfo?.names.includes("")){
+        if (this.state.kidinfo?.names.includes("")) {
             this.setState({errormsg: "None of the kid's names can be blank"})
             return;
         }
-        if(Number(this.state.kids) < 0){
+        if (Number(this.state.kids) < 0) {
             this.setState({errormsg: "Can't have negative kids"})
             return;
         }
         save({
-            name: this.state.name, 
-            kids: Number(this.state.kids), 
+            name: this.state.name,
+            kids: Number(this.state.kids),
             allergies: this.state.allergies,
             plusone: this.state.plusone,
             plusoneinfo: this.state.plusoneinfo,
@@ -140,42 +145,50 @@ export class AddGuest extends Component<AgProps, AgState> {
 
     updateKidAllergies = (evt: ChangeEvent<HTMLTextAreaElement>): void => {
         let newAlls: string[] | undefined = this.state.kidinfo?.allergies.slice(0)
-        if(newAlls === undefined) throw new Error("y is kidinfo undefined");
-        newAlls[Number(evt.target.name)] = evt.target.value 
-        this.setState({kidinfo: {
-            allergies: newAlls,
-            names: (this.state.kidinfo !== undefined) ? this.state.kidinfo?.names : []
-        }})
+        if (newAlls === undefined) throw new Error("y is kidinfo undefined");
+        newAlls[Number(evt.target.name)] = evt.target.value
+        this.setState({
+            kidinfo: {
+                allergies: newAlls,
+                names: (this.state.kidinfo !== undefined) ? this.state.kidinfo?.names : []
+            }
+        })
     }
 
     updateKidName = (evt: ChangeEvent<HTMLInputElement>): void => {
         let newNames: string[] | undefined = this.state.kidinfo?.names.slice(0)
-        if(newNames === undefined) throw new Error("y is kidinfo undefined");
+        if (newNames === undefined) throw new Error("y is kidinfo undefined");
         console.log(`newNames: ${newNames}`)
-        newNames[Number(evt.target.name)] = evt.target.value 
-        this.setState({kidinfo: {
-            names: newNames,
-            allergies: (this.state.kidinfo !== undefined) ? this.state.kidinfo?.allergies : []
-        }})
+        newNames[Number(evt.target.name)] = evt.target.value
+        this.setState({
+            kidinfo: {
+                names: newNames,
+                allergies: (this.state.kidinfo !== undefined) ? this.state.kidinfo?.allergies : []
+            }
+        })
     }
 
     updatePoAllergies = (evt: ChangeEvent<HTMLTextAreaElement>): void => {
-        this.setState({plusoneinfo: {
-            allergies: evt.target.value,
-            name: (this.state.plusoneinfo !== undefined) ? this.state.plusoneinfo?.name : ""
-        }})
+        this.setState({
+            plusoneinfo: {
+                allergies: evt.target.value,
+                name: (this.state.plusoneinfo !== undefined) ? this.state.plusoneinfo?.name : ""
+            }
+        })
     }
 
     updatePoName = (evt: ChangeEvent<HTMLInputElement>): void => {
-        this.setState({plusoneinfo: {
-            name: evt.target.value,
-            allergies: (this.state.plusoneinfo !== undefined) ? this.state.plusoneinfo?.allergies : ""
-        }})
+        this.setState({
+            plusoneinfo: {
+                name: evt.target.value,
+                allergies: (this.state.plusoneinfo !== undefined) ? this.state.plusoneinfo?.allergies : ""
+            }
+        })
     }
 
     updateKids = (evt: ChangeEvent<HTMLInputElement>): void => {
         const n: number = Number(evt.target.value);
-        if(n > KID_LIMIT){
+        if (n > KID_LIMIT) {
             this.setState({errormsg: "Please stop having kids"})
             return;
         }
