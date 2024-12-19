5e4dd22b5413c228ec6be4113bfd2a59f6568bdb
karlabelem <kbarrientosguevara@gmail.com>
2024-12-18 20:19:38 -0800

    Big oopsie with CSS styling lol, fixed it in App.tsx

--- nextjs-blog/public/Home.tsx
+++ nextjs-blog/public/Home.tsx
@@ -7,15 +7,16 @@ type Props = {
 
 export const Home: React.FC<Props> = ({ openPage }) => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.heading}>Welcome to the Event Planner!</h2>
-      <p className={styles.description}>
-        Organize, manage, and enjoy stress-free event planning with just a few clicks.
+    <div>
+        <h2 className={styles.wordContent}>Welcome to the Event Planner!
+        </h2>
+        <p className={styles.wordContent}>
+            Organize, manage, and enjoy stress-free event planning with just a few clicks
       </p>
-      <div className={styles.navigation}>
-        <button className={styles.navLink} onClick={() => openPage("guestlist")}>Guest list </button>
-        <button className={styles.navLink} onClick={() => openPage("addguest")}>Add guest</button>
-      </div>
+        <div>
+            <button onClick={() => openPage("guestlist")}>Guest list</button>
+            <button onClick={() => openPage("addguest")}>Add guest</button>
+        </div>
     </div>
   );
 };
